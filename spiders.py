2dcb244b5dba2479d77dd63782409881b0e52785
Flash1105-2 <flashpay110599@gmail.com>
2023-08-21 21:17:52 -0400

    add addtional spiders to list

--- spiders.py
+++ spiders.py
@@ -9,8 +9,17 @@ class Spider:
 
 spiders = [
         Spider("Black Widow", "Latrodectuss spp.", "Worldwide", "Insects", "Venomous", True),
-
+        Spider("Wolf Spider", "Lycosidae", "Worldwide", "Insects and small vertebrates", "Hunting", False),
+        Spider("Golden Silk Orb Weaver", "Nephila spp.", "Tropical regions", "Flying insects", "Orb-weaver", False),
+        Spider("Brown Widow", "Latrodectus geometricus", "Worldwide", "Insects", "Venomous", True),
+        Spider("Brazilian Wandering Spider", "Phoneutria spp.", "South America", "Insects and small vertebrates", "Active hunter", True),
+        Spider("Trapdoor Spider", "Ctenizidae", "Worldwide", "Insects and small vertebrates", "Ambush predator", False),
+        Spider("Fishing Spider", "Dolomedes spp.", "North and South America", "Aquatic insects and small fish", "Semi-aquatic", False),
+        Spider("Goliath Birdeater", "Theraphosa blondi", "South America", "Insects and small vertebrates", "Burrowing", False),
+        Spider("Redback Spider", "Latrodectus hasselti", "Australia", "Insects", "Venomous", True),
+        Spider("Camel Spider", "Solifugae", "Arid regions", "Insects and small vertebrates", "Fast runner", False),
 ]
+
 
 spider_details = {
     spider.name: {
