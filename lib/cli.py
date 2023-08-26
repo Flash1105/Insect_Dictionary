12b8021bb0d63ac9cf688301c6c70cbcd5b72c33
Flash1105-2 <flashpay110599@gmail.com>
2023-08-26 00:37:40 -0400

    aadd database setup and proper labeling for detail

--- lib/cli.py
+++ lib/cli.py
@@ -3,12 +3,20 @@ from sqlalchemy import create_engine
 from data.database import DATABASE_URL
 from data.model import InsectTable, SpiderTable
 
-def display_insect_details(selected_insect):
+#setup for database
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def display_insect_details(animal):
     print("\nSelected Insect:")
-    print(f"Name: {selected_insect.name}")
-    print(f"Scientific Name: {selected_insect.scientific_name}")
-    print(f"Habitat: {selected_insect.habitat}")
-    print(f"Diet: {selected_insect.diet}")
-    print(f"Behavior: {selected_insect.behavior}")
-    print()
+    print(f"Name: {animal.name}")
+    print(f"Scientific Name: {animal.scientific_name}")
+    print(f"Habitat: {animal.habitat}")
+    print(f"Diet: {animal.diet}")
+    print(f"Behavior: {animal.behavior}")
+    if isinstance(animal, SpiderTable):
+        print(f"Venomous: {'Yes' if animal.venomous else 'No'}")
+
 
