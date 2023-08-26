253df2afee1e8da8a93d23bf2b0d64954477b163
Flash1105-2 <flashpay110599@gmail.com>
2023-08-26 19:20:47 -0400

    remove imports and main logic

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-from Insects import insects
-from spiders import Spiders
-
 from lib.cli import main_menu, display_insect_details, display_spider_list, display_insect_list 
 from data.database import session, initialize_database
-from data.model import InsectTable, SpiderTable
 
 def main():
     
     initialize_database(session)  
-
-    while True:
-        main_menu(insects)
-
-        try:
-            choice = int(input("Enter the number of an insect to learn more (0 to exit): "))
-            if choice == 0:
-                break
-
-            elif 1 <= choice <= len(insects):
-                selected_insect = insects[choice - 1]
-                insect = session.query(InsectTable).filter_by(name=selected_insect.name).first()
-                if insect:
-                    display_insect_details(insect)
-                else:
-                    print("Insect details not found in the database.")
-            else:
-                print("Invalid choice. Please enter a valid number.\n")
-        except ValueError:
-            print("Invalid input. Please enter a number.\n")
-    
+    main_menu()
     session.close()
 
 if __name__ == "__main__":
