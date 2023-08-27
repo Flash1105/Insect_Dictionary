c38c6fe47561b4482e9f66225aa4b3bfcb6a58ae
Flash1105-2 <flashpay110599@gmail.com>
2023-08-27 00:24:58 -0400

    prereqs

--- data/model.py
+++ data/model.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
+# Define Bases as tables for both 
 class InsectTable(Base):
     __tablename__ = 'insects'
     id = Column(Integer, primary_key=True)
