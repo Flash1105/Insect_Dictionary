6fc4038b4378eb313e82da361d434fe6d5cd8715
Flash1105-2 <flashpay110599@gmail.com>
2023-08-29 21:22:50 -0400

    clean up code

--- data/model.py
+++ data/model.py
@@ -1,35 +1,6 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
-
-# Define Bases as tables for both 
-class InsectTable(Base):
-    __tablename__ = 'insects'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    scientific_name = Column(String)
-    habitat = Column(String)
-    diet = Column(String)
-    behavior = Column(String)
-
-    # One-to-many relationship with facts
-    facts = relationship ("AnimalFact", back_populates="insect")
-
-
-class SpiderTable(Base):
-    __tablename__ = 'spiders'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    scientific_name = Column(String)
-    habitat = Column(String)
-    diet = Column(String)
-    behavior = Column(String)
-    venomous = Column(Boolean)
-
-    facts = relationship("AnimalFact", back_populates="spider")
+from .database import Base
 
 class AnimalFact(Base):
     __tablename__ = 'animal_facts'
@@ -40,3 +11,22 @@ class AnimalFact(Base):
     spider_id = Column(Integer, ForeignKey('spiders.id'))
     spider = relationship("SpiderTable", back_populates="facts")
 
+class InsectTable(Base):
+    __tablename__ = 'insects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    scientific_name = Column(String)
+    habitat = Column(String)
+    diet = Column(String)
+    behavior = Column(String)
+    facts = relationship("AnimalFact", back_populates="insect")
+
+class SpiderTable(Base):
+    __tablename__ = 'spiders'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    scientific_name = Column(String)
+    habitat = Column(String)
+    diet = Column(String)
+    behavior = Column(String)
+    facts = relationship("AnimalFact", back_populates="spider")
