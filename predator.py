87a8a4ae5437dc543fafbfe9344532d0692200cf
Flash1105-2 <flashpay110599@gmail.com>
2023-08-30 00:10:22 -0400

    create predator list

---
+++ predator.py
@@ -0,0 +1,43 @@
+class Predator: 
+    def __init__ (self, name):
+        self.name = name 
+
+Predators = [
+    Predator("Birds"),
+    Predator("Bats"),
+    Predator("Lizards"),
+    Predator("Frogs"),
+    Predator("Spiders"),
+    Predator("Wasps"),
+    Predator("Praying mantises"),
+    Predator("Beetles"),
+    Predator("Ants"),
+    Predator("Dragonflies"),
+    Predator("Centipedes"),
+    Predator("Scorpions"),
+    Predator("Turtles"),
+    Predator("Fish"),
+    Predator("Small mammals"),
+    Predator("Carnivorous plants"),
+    Predator("Other insects"),
+    Predator("Hedgehogs"),
+    Predator("Shrews"),
+    Predator("Moles"),
+    Predator("Squirrels"),
+    Predator("Foxes"),
+    Predator("Snakes"),
+    Predator("Mice"),
+    Predator("Raccoons"),
+    Predator("Skunks"),
+    Predator("Weasels"),
+    Predator("Owls"),
+    Predator("Hawks"),
+    Predator("Vultures")
+]
+
+predator_details = {
+    predator.name: {
+
+    }
+    for predator in Predators
+}
