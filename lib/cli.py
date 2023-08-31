fa002b1d049ccb897e61bdefd68e296ec9cb6c27
Flash1105-2 <flashpay110599@gmail.com>
2023-08-31 15:48:34 -0400

    print for predators

--- lib/cli.py
+++ lib/cli.py
@@ -60,7 +60,8 @@ def display_insect_details(animal):
     print(f"Behavior: {animal.behavior}")
     if isinstance(animal, SpiderTable):
         print(f"Venomous: {'Yes' if animal.venomous else 'No'}")
-    print (f"Predators: {', '.join(animal.predators)}")
+    print (f"Predators: {', '.join(p.name for p in animal.predator)}")
+
 def display_spider_details(spider):
     print("\nSelected Spider:")
     print(f"Name: {spider.name}")
@@ -69,6 +70,7 @@ def display_spider_details(spider):
     print(f"Diet: {spider.diet}")
     print(f"Behavior: {spider.behavior}")
     print(f"Venomous: {'Yes' if spider.venomous else 'No'}")
+    print (f"Predators: {','.join(p.name for p in spider.predators)}")
 
 def display_insect_list():
     print("Available Animals:")
