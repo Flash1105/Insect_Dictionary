e0d54492b14c556d857ff2d60b8984d77ef088b1
Flash1105-2 <flashpay110599@gmail.com>
2023-08-31 19:37:16 -0400

    print predators

--- data/model.py
+++ data/model.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 #association for many-to-many relationship
